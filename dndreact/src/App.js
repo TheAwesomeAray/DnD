dfe9588825e0d5d61e4d0c241ef7af4fb3ce5757
TheAwesomeARay <andrewray4@gmail.com>
2019-01-31 21:02:38 -0600

    React Router and Redux

--- dndreact/src/App.js
+++ dndreact/src/App.js
@@ -1,34 +1,56 @@
 import React, { Component } from 'react';
 import './App.css';
-import MainMenu from './MainMenu';
 import CharacterMenu from './CharacterMenu';
+import { connect } from 'react-redux';
 
-function initialCharacters() {
-  return [
-  { name: 'Ilin' },
-  { name: 'Kagor' },
-  { name: 'Bellocke' }]
-};
+let state;
+const App = connect(mapStateToProps, mapDispatchToProps)(
+  function ({characters, characterAdded, onCharacterAdded}) {
+    console.log(characterAdded);
+  return (
+    <div className="App">
+      <Menu />
+      <CharacterMenu  characterAdded={characterAdded}
+                      characters={characters}
+                      AddCharacter={onCharacterAdded} />
+    </div>
+  );
+});
 
-class App extends Component {
-  state = {
-    characters: initialCharacters(),
-    characterAdded: false
+const Menu = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <a className="navbar-brand" href="#">Dungeons and Dragons</a>
+     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item active">
+            <a className="nav-link" href="/">Character Creation <span class="sr-only">(current)</span></a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="/StoryMode">Story Mode</a>
+          </li>
+        </ul>
+  </div>
+  
+  </nav>
+  );
+}
+
+function mapStateToProps(state) {
+  return {
+    characters: state.characters,
+    characterAdded: state.characterAdded
   }
-  AddCharacter = (character) => {
-    this.setState((prevState) => ({
-      characters: prevState.characters.concat([character]),
-      characterAdded: true
-    }));
-    
-  };
-  render() {
-    return (
-      <div className="App">
-        <CharacterMenu {...this.state} 
-                       AddCharacter={this.AddCharacter} />
-      </div>
-    );
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    onCharacterAdded: (character) => {
+      dispatch({type: 'CHARACTER_ADDED', character})
+    }
   }
 }
 
--- dndreact/src/index.js
+++ dndreact/src/index.js
@@ -3,8 +3,62 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {BrowserRouter, Route} from 'react-router-dom';
+import * as Redux from 'redux';
+import * as ReactRedux from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const characters = [
+    { name: 'Ilin' },
+    { name: 'Kagor' },
+    { name: 'Bellocke' }
+];
+
+const characterAdded = false;
+
+function reducer(state = {characters, characterAdded},
+action) {
+    switch (action.type) {
+        case 'CHARACTER_ADDED':
+            return Object.assign(
+                {}, 
+                state, {
+                    characters: state.characters.concat([action.character]),
+                    characterAdded: true
+            });
+        default: return state;
+    }
+}
+ 
+function logger({ getState }) {
+    return next => action => {
+      //console.log('will dispatch', action)
+  
+      // Call the next dispatch method in the middleware chain.
+      const returnValue = next(action)
+  
+      //console.log('state after dispatch', getState())
+  
+      // This will likely be the action itself, unless
+      // a middleware further in chain changed it.
+      return returnValue
+    }
+  }
+
+let store = Redux.createStore(reducer, composeWithDevTools(
+    Redux.applyMiddleware(logger)
+));
+
+ReactDOM.render(
+    <BrowserRouter>
+        <React.Fragment>
+            <ReactRedux.Provider store={store}>
+                <Route exact path="/" component={App}/>
+                <Route exact path="/StoryMode" component={App}/>
+            </ReactRedux.Provider>
+        </React.Fragment>
+    </BrowserRouter>
+, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
